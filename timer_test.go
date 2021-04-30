5739bab60076bca9ea89f6be700a2b6760a27553
zajann <zajan.ksk@gmail.com>
2021-04-30 11:04:32 +0900

    fix test code

--- timer_test.go
+++ timer_test.go
@@ -5,7 +5,21 @@ import (
 	"time"
 )
 
-func BenchmarkTimer(b *testing.B) {
+func BenchmarkGetFormatStringWithTimer(b *testing.B) {
+	Init(0)
+
+	for i := 0; i < b.N; i++ {
+		GetTimeFormat("2006-01-02 15:04:05")
+	}
+}
+
+func BenchmarkGetFormatStringWithoutTimer(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		time.Now().Format("2006-01-02 15:04:05")
+	}
+}
+
+func BenchmarkGetMonthStringWithTimer(b *testing.B) {
 	Init(0)
 
 	for i := 0; i < b.N; i++ {
@@ -13,7 +27,7 @@ func BenchmarkTimer(b *testing.B) {
 	}
 }
 
-func BenchmarkOriginTime(b *testing.B) {
+func BenchmarkGetMonthStringWithoutTimer(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		time.Now().Month().String()
 	}
